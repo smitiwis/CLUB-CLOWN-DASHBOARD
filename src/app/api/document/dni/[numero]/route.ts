b7edf120c17faad4cd3633d6ac4dc850f75d74b8
smitiwis <sistemas.luismi@gmail.com>
2025-01-14 13:49:07 -0500

    corregir error de obtener numero de dni del parametro

--- src/app/api/document/dni/[numero]/route.ts
+++ src/app/api/document/dni/[numero]/route.ts
@@ -2,24 +2,25 @@ import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 type params = {
-  params: {
-    numero: string;
-  };
+  params: Promise<{ numero: string }>;
 };
 
-export async function GET(req: NextRequest, { params }: params) {
-
+export async function GET(request: NextRequest, { params }: params) {
   const path = process.env.NEXT_PATH_DOC;
   const token = process.env.NEXT_TOKEN_DOC;
+  const nroDni = (await params).numero;
+
   try {
-    const response = await axios.get(
-      `${path}/dni/${params.numero}?token=${token}`
-    );
+    if (!nroDni) {
+      return NextResponse.json(
+        { message: { error: "Debe ingresar un número de documento" } },
+        { status: 404 }
+      );
+    }
+    const response = await axios.get(`${path}/dni/${nroDni}?token=${token}`);
     if (!response.data.success) {
       return NextResponse.json(
-        {
-          message: response.data.message,
-        },
+        { message: response.data.message },
         { status: 404 }
       );
     }
@@ -27,11 +28,7 @@ export async function GET(req: NextRequest, { params }: params) {
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      {
-        message: {
-          error: "No se encontró el documento",
-        },
-      },
+      { message: { error: "No se encontró el documento" } },
       { status: 404 }
     );
   }
